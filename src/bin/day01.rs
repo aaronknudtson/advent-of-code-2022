1d6cfec89738d0634c15927a20cc65bbcd7c5a32
aaronknudtson <aaron.knudtson@outlook.com>
2022-12-01 08:22:27 -0500

    day01 part 2 complete

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -5,9 +5,13 @@ fn sum_calories(cals: &str) -> u32 {
 fn main() {
     let elves = std::fs::read_to_string("src/day01.txt").unwrap();
     let elf_list: Vec<_> = elves.split("\n\n").collect();
-    let elf_cal_list = elf_list.iter().fold(Vec::new(), |mut cals, elf_cal| {
+    let mut elf_cal_list = elf_list.iter().fold(Vec::new(), |mut cals, elf_cal| {
         cals.push(sum_calories(elf_cal));
         cals
     });
-    println!("Part A: {:?}", elf_cal_list.iter().max())
+    println!("Part one: {:?}", elf_cal_list.iter().max().unwrap());
+
+    elf_cal_list.sort();
+    println!("Part two: {:?}", elf_cal_list.iter().rev().take(3).sum::<u32>())
+
 }
