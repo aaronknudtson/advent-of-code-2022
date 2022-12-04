f4a22b992ca65c16cd8988fe091767d441d9ef5c
aaronknudtson <aaron.knudtson@outlook.com>
2022-12-04 13:42:55 -0500

    day04 part two completed

--- src/bin/day04.rs
+++ src/bin/day04.rs
@@ -22,20 +22,30 @@ impl Range {
     fn is_contained_by(&self, other: &Range) -> bool {
         self.lo >= other.lo && self.hi <= other.hi
     }
+
+    fn is_overlapped_by(&self, other: &Range) -> bool {
+        !(self.lo > other.hi || other.lo > self.hi)
+    }
 }
 
 fn main() {
     let input = std::fs::read_to_string("src/day04.txt").unwrap();
-    println!("lines len: {}", input.lines().count());
+
     let range_pairs: Vec<_> = input
         .lines()
         .map(|l| l.split(',').map(|s| Range::from_str(s).unwrap()).collect::<Vec<_>>())
         .collect();
-
     let num_fully_contained = range_pairs.iter().filter(|pair| {
         let p1 = &pair[0];
         let p2 = &pair[1];
         p1.is_contained_by(p2) || p2.is_contained_by(p1)
     }).count();
     println!("Part one: {}", num_fully_contained);
+
+    let num_partially_contained = range_pairs.iter().filter(|pair| {
+        let p1 = &pair[0];
+        let p2 = &pair[1];
+        p1.is_overlapped_by(p2)
+    }).count();
+    println!("Part two: {}", num_partially_contained);
 }
