445cccf9a5a8e3e0bce44e0dd612af57902e3823
aaronknudtson <aaron.knudtson@outlook.com>
2022-12-16 09:41:18 -0500

    day6 part two

--- src/bin/day06.rs
+++ src/bin/day06.rs
@@ -1,19 +1,22 @@
 use std::collections::HashSet;
 
+fn get_n_unique(input: &Vec<char>, n: usize) -> usize {
+    let mut windows = input.windows(n);
+    let mut count = n;
+    while let Some(elems) = windows.next() {
+        let mut hs = HashSet::<&char>::new();
+        if elems.iter().map(|v| hs.insert(v)).all(|v| v == true) {
+            break;
+        }
+        count += 1;
+    }
+    return count;
+}
 fn main() {
     let input: Vec<_> = std::fs::read_to_string("src/day06.txt")
         .unwrap()
         .chars()
         .collect();
-    let mut windows = input.windows(4);
-    let mut count = 4;
-    while let Some(elems) = windows.next() {
-        let mut hs = HashSet::<&char>::new();
-        if elems.iter().map(|v| hs.insert(v)).all(|v| v == true) {
-            println!("{:?}", elems);
-            println!("Part one: {}", count);
-            break;
-        }
-        count += 1;
-    }
+    println!("Part one: {}", get_n_unique(&input, 4));
+    println!("Part two: {}", get_n_unique(&input, 14));
 }
